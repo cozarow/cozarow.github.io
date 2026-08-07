d2dafcf04f75d91b954e08cf1d970414fbc4a58f
cerulean ozarow <ceruleanozarow@mac.mynetworksettings.com>
2026-08-06 21:59:17 -0400

    made pronunciation hovering work, updated personal description, etc

--- javascript/header.js
+++ javascript/header.js
@@ -1,5 +1,6 @@
 function showHeader() {
   const header = document.getElementById("site-header");
+  header.style.transition = "opacity 1s ease";
   header.style.opacity = "1";
 }
 
--- javascript/loading_two.js
+++ javascript/loading_two.js
@@ -1,10 +1,15 @@
 function fadeText2() {
   const textElement = document.getElementById("text2");
   textElement.style.transition = "color 4s ease";
+
+// Allow header to appear while still transitioning
+  setTimeout(() => {
+    document.dispatchEvent(new Event("secondDone"));
+  }, 2000);
+
   requestAnimationFrame(() => {
     textElement.style.color = "white";
   });
 }
 
-// wait for first.js to signal it's done, instead of using window.onload
 document.addEventListener("firstDone", fadeText2);
